8f641a6b1f7b6292f7a187e8809ca799d529abd3
Smyk <smyk006@yandex.ru>
2018-03-13 05:30:04 +0300

    step 3

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-var namber = new Array();
-var name = new Array();
-var fName = new Array();
-$("#btnAdd").mousedown(function() {
+// var number = [];
+// var name = [];
+// var fName = [];
+// var count = 0;
 
-})
+// Код для понимания:
+// $("#btnAdd").click(function(){
+//     x = ($("#name").val());
+//     var fname = ($("#fName").val());
+//     alert("Имя: " + x + " Фамилия: " + fname);
+//   });
+
+var x = 1;
+
+$("#btnAdd").click(function(){
+    $("#tbodyPlus").append(
+      "<tr> <td>" + x + "</td> <td>" + $("#name").val() + "</td> <td>" + $("#fName").val() + "</td> <td> <button class='btn btn-secondary btn_size btnDel' type='button'><i class='material-icons'>clear</i></button></td></tr>"
+    );
+    $(".textAdd").val("");
+    x ++;
+
+    $(".btnDel").click(function(){
+        $(this).closest('tr').remove();
+      });
+});
