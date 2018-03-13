1414c7a2929f3ce7a27b3e19d78eb9bf325893a4
Smyk <smyk006@yandex.ru>
2018-03-13 19:57:19 +0300

    test

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// var number = [];
-// var name = [];
-// var fName = [];
-// var count = 0;
-
 // Код для понимания:
 // $("#btnAdd").click(function(){
 //     x = ($("#name").val());
@@ -10,16 +5,43 @@
 //     alert("Имя: " + x + " Фамилия: " + fname);
 //   });
 
-var x = 1;
+$(document).ready(function () {
+  var x = 1;
+  var validName = false;
+  var validFname = false;
 
-$("#btnAdd").click(function(){
-    $("#tbodyPlus").append(
-      "<tr> <td>" + x + "</td> <td>" + $("#name").val() + "</td> <td>" + $("#fName").val() + "</td> <td> <button class='btn btn-secondary btn_size btnDel' type='button'><i class='material-icons'>clear</i></button></td></tr>"
-    );
-    $(".textAdd").val("");
-    x ++;
+  $(".input-group").click(function (event) {
+    var name = $("#name").val();
+    var fName = $("#fName").val();
+    console.log(name)
+    if (name == "") {
 
-    $(".btnDel").click(function(){
-        $(this).closest('tr').remove();
+    } else {
+      validName = true;
+    }
+    if (fName == "") {
+
+    } else {
+      validFname = true;
+    }
+
+    if (validName == true && validFname == true) {
+      $("#btnAdd").click(function () {
+        $("#tbodyPlus").append(
+          "<tr> <td id='number'>" + x + "</td> <td>" + $("#name").val() + "</td> <td>" + $("#fName").val() + "</td> <td> <button class='btn btn-secondary btn_size btnDel' type='button'><i class='material-icons'>clear</i></button></td></tr>"
+        );
+        $(".textAdd").val("");
+        x++;
+        throw new Error();
       });
+    } else {
+      throw new Error();
+    }
+    $(".btnDel").click(function () {
+      $(this).closest('tr').remove();
+      x--;
+      throw new Error();
+    });
+    return
+  });
 });
