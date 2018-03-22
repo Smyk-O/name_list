99ed78e0d5503003dac900c8a832c1e85fb6fa17
Smyk <smyk006@yandex.ru>
2018-03-22 18:15:59 +0300

    step 7

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
 
   var list_item = [];
+  var $btnAdd = $("#btnAdd");
 
-  $(function ($) {
-    list_item = localStorage.save_list_data ? JSON.parse(localStorage.save_list_data) : [];
+  if (localStorage.save_list_data) {
+    list_item = JSON.parse(localStorage.save_list_data);
     updateView();
-  });
+  }
 
-  $(function ($) {
-    $("#phone").mask("+7(999) 999-9999");
-  });
+  $("#phone").mask("+7 (999) 999-9999");
 
-  $("#btnAdd").popover({
+
+  $btnAdd.popover({
     content: 'Введите все данные',
     placement: 'top',
     trigger: 'manual'
@@ -23,7 +23,7 @@ $(document).ready(function () {
     }
   });
 
-  $("#btnAdd").click(function (event) {
+  $btnAdd.click(function (event) {
     valid();
   });
 
@@ -36,31 +36,59 @@ $(document).ready(function () {
 
   });
 
-  $("#btnSortName").click(function (event) {
-    list_item.sort(compareName);
-    updateView();
+  $("#btnSortName").click(function () {
+    compare($(this), 'name');
   });
 
-  $("#btnSortLname").click(function (event) {
-    list_item.sort(compareLname);
-    updateView();
+  $("#btnSortLname").click(function () {
+    compare($(this), 'lastname');
   });
 
-  $("#btnSortPhone").click(function (event) {
-    list_item.sort(comparePhone);
-    updateView();
+  $("#btnSortPhone").click(function () {
+    compare($(this), 'phone');
   });
+
+  function compare($this, keyName) {
+    cleanClass($this)
+
+    if (!$this.hasClass("asc")) {
+      $this.removeClass("desc")
+      $this.addClass("asc");
+
+      list_item.sort(function (a, b) {
+        return a[keyName] > b[keyName];
+      });
+
+    } else {
+      $this.removeClass("asc")
+      $this.addClass("desc");
+
+      list_item.sort(function (a, b) {
+        return a[keyName] < b[keyName];
+      });
+
+    };
+
+    updateView();
+  }
+
+  function cleanClass($this) {
+    $(".table th").each(function () {
+      if ($this.attr('id') !== $(this).attr('id')) {
+        $(this).removeClass('asc desc');
+      };
+    });
+  }
 
   function valid(event) {
     var name = $("#name").val();
     var lastname = $("#lname").val();
     var phone = $("#phone").val();
 
-
     if (!name || !lastname || !phone) {
-      $("#btnAdd").popover("show");
+      $btnAdd.popover("show");
       setTimeout(function () {
-        $("#btnAdd").popover('hide');
+        $btnAdd.popover('hide');
       }, 2000);
       return true;
     }
@@ -74,13 +102,15 @@ $(document).ready(function () {
   }
 
   function addtolist_item(name, lastname, phone) {
+    var data = times()
     list_item.push({
       name: name,
       lastname: lastname,
-      phone: phone
+      phone: phone,
+      addata: data
     });
     updateView();
-  };
+  }
 
   function updateView() {
 
@@ -88,7 +118,6 @@ $(document).ready(function () {
 
     list_item.forEach(function (item, index) {
       var actualIndex = index + 1;
-
       var tb_str = "<tr>";
       tb_str += "<td>" + actualIndex + "</td>";
       tb_str += "<td>" + item.name + "</td>";
@@ -104,21 +133,21 @@ $(document).ready(function () {
     save_list(list_item)
     $("#tbodyPlus").html(tb_plus);
     $(".textAdd").val("");
-  };
+  }
+
+
+  function save_list(save_list_data) {
+    localStorage.save_list_data = JSON.stringify(save_list_data);
+  }
+
+  function times() {
+    var data = new Date();
+    var tame = data.getDate() + ".";
+    tame += data.getMonth() + ".";
+    tame += data.getFullYear() + "г.";
+    tame += data.getHours() + ":";
+    tame += data.getMinutes() + ":";
+    tame += data.getSeconds();
+    return tame;
+  }
 });
-
-function save_list(save_list_data) {
-  localStorage.save_list_data = JSON.stringify(save_list_data);
-};
-
-function compareName(nameA, nameB) {
-  return nameA.name > nameB.name;
-};
-
-function compareLname(lnameA, lnameB) {
-  return lnameA.lastname > lnameB.lastname;
-};
-
-function comparePhone(phoneA, phoneB) {
-  return phoneA.phone > phoneB.phone;
-};
